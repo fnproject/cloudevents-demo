b57b075cab1d92ac9f2f1b2cfd8af596c29b6c35
Chad Arimura <carimura@gmail.com>
2018-04-30 14:52:28 -0700

    spacing

---
+++ setenv.sh
@@ -0,0 +1,6 @@
+fn routes config set cloudevents image-processor TWITTER_CONSUMER_KEY $TWITTER_CONF_KEY
+fn routes config set cloudevents image-processor TWITTER_CONSUMER_SECRET $TWITTER_CONF_SECRET
+fn routes config set cloudevents image-processor TWITTER_ACCESS_TOKEN_KEY $TWITTER_TOKEN_KEY
+fn routes config set cloudevents image-processor TWITTER_ACCESS_TOKEN_SECRET $TWITTER_TOKEN_SECRET
+fn routes config set cloudevents image-processor SLACK_API_TOKEN $SLACK_API_TOKEN
+fn routes config set cloudevents image-processor SLACK_CHANNEL $SLACK_CHANNEL

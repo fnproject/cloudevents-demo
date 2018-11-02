b4f7e9541ded9471b1fc60e156860942a2a2657d
Denis Makogon <lildee1991@gmail.com>
2018-11-02 18:03:27 +0200

    scripts

---
+++ functions/word-generator/call_binary.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+curl -v -X POST -H 'ce-cloudeventsversion: 0.1' \
+    -H 'ce-eventtime: 2018-10-23T12:28:22.4579346Z' \
+    -H 'ce-eventid: 96fb5f0b-001e-0108-6dfe-da6e2806f124' \
+    -H 'ce-source: http://srcdog.com/cedemo' \
+    -H 'ce-eventtype: word.found.noun' \
+    -H 'content-type: application/json' `fn inspect context | grep api | awk '{print $2}'`/t/cncf/word-generator-trigger

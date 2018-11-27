71aba8d7dae2fd3453815f1f7069f912a9d1ea57
Denis Makogon <lildee1991@gmail.com>
2018-11-27 13:48:02 +0200

    Update env file

--- oci-proxy/env.sh
+++ oci-proxy/env.sh
@@ -5,6 +5,7 @@ export OCI_TENANCY=`cat ~/.oci/config | grep tenancy | awk '{split($0,array,"=")
 export OCI_REGION=`cat ~/.oci/config | grep region | awk '{split($0,array,"=")} END{print array[2]}'`
 export OCI_FINGERPRINT=`cat ~/.oci/config | grep fingerprint | awk '{split($0,array,"=")} END{print array[2]}'`
 export OCI_KEY=`cat $(cat ~/.oci/config | grep key_file | awk '{split($0,array,"=")} END{print array[2]}')`
+export OCI_COMPARTMENT=${OCI_COMPARTMENT:-`cat ~/.oci/config | grep compartment_id | awk '{split($0,array,"=")} END {print array[2]}'`}
 export OCI_KEY_PASS=${1}
-export OCI_COMPARTMENT=${2}
-export FN_INVOKE_ENDPOINT=${3}
+
+export FN_INVOKE_ENDPOINT=${2}
